e78551fb60bbc8318ead87360eb9760f696755c3
Axel Whitford <axel@tanda.co>
2022-02-06 10:06:53 +1000

    tailwindcss installed

--- src/little-parliament-frontend/src/components/Welcome.jsx
+++ src/little-parliament-frontend/src/components/Welcome.jsx
@@ -1,9 +1,11 @@
+import "../styles/home.css"
+
 export default function Welcome() {
     return (
-        <div className="hero-image h-screen">
+        <div className="hero-image">
             <div class="hero-text">
                 <h1>Little Parliament</h1>
-                
+
             </div>
         </div>
     )
--- src/little-parliament-frontend/src/pages/Home.jsx
+++ src/little-parliament-frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection';
 import Welcome from '../components/Welcome';
+
 export default function Home() {
 
   return (
