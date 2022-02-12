6e4defa6a47ec3c10e1a30ee00354b6bf21ce27c
Axel Whitford <axel@tanda.co>
2022-02-12 10:09:51 +1000

    pulled branch and cleaned

--- src/little-parliament-frontend/src/pages/Home.jsx
+++ src/little-parliament-frontend/src/pages/Home.jsx
@@ -1,25 +1,8 @@
 import React from 'react';
-import HeroSection from '../components/Home/HeroSection';
-import CoffeeSection from '../components/Home/CoffeeSection';
-import FoodSection from '../components/Home/FoodSection';
-import OurStoreSection from '../components/Home/OurStoreSection';
-import Welcome from '../components/Home/Welcome';
-import "../styles/home.css"
 
 export default function Home() {
-
   return (
-    <>
-      <Welcome />
-      {/* About us */}
-      <HeroSection />
-      {/* Coffee */}
-      <CoffeeSection />
-      {/* Menu */}
-      <FoodSection />
-      {/* Products */}
-      <OurStoreSection />
-    </>
+      <h1>Home</h1>
   )
 }
 
--- src/little-parliament-frontend/src/pages/Menu.jsx
+++ src/little-parliament-frontend/src/pages/Menu.jsx
@@ -1,16 +1,7 @@
 import React from 'react';
-import MenuSelector from '../components/Menu/MenuSelector';
-import '../styles/menu.css'
 
 export default function Menu() {
   return (
-      <>
-        {/* Menu Selector */}
-        <MenuSelector />
-        {/* Drinks */}
-        {/* Cabinet */}
-        {/* Breakfast */}
-        {/* Lunch */}
-      </>
+      <h1>Menu</h1>
   )
 }
