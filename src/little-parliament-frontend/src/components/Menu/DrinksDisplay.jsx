daa6c2a399844d6ec69949ceb74b509020ed5d9e
Axel Whitford <axel@tanda.co>
2022-02-12 13:52:42 +1000

    fixed loading times lol

--- src/little-parliament-frontend/src/components/Menu/DrinksDisplay.jsx
+++ src/little-parliament-frontend/src/components/Menu/DrinksDisplay.jsx
@@ -2,16 +2,16 @@ import MenuItem from "./MenuItem"
 import Grid from '@mui/material/grid'
 
 const menuItems = [
-    { id: 1, name: 'Flat White', description: 'Smashed avo on a deliscious Sourdough toast', price: 5, image: 'https://picsum.photos/206' },
-    { id: 2, name: 'Latter', description: 'Smashed avo on a deliscious Sourdough toast', price: 5, image: 'https://picsum.photos/207' },
-    { id: 3, name: 'Cappuchino', description: 'Smashed avo on a deliscious Sourdough toast', price: 5, image: 'https://picsum.photos/208' },
-    { id: 4, name: 'Long Black', description: 'Smashed avo on a deliscious Sourdough toast', price: 4, image: 'https://picsum.photos/209' },
-    { id: 5, name: 'Espresso', description: 'Smashed avo on a deliscious Sourdough toast', price: 3, image: 'https://picsum.photos/210' },
-    { id: 6, name: 'Doppio', description: 'Smashed avo on a deliscious Sourdough toast', price: 6, image: 'https://picsum.photos/211' },
-    { id: 7, name: 'Iced Latte', description: 'Smashed avo on a deliscious Sourdough toast', price: 5.5, image: 'https://picsum.photos/212' },
-    { id: 8, name: 'Iced Long black', description: 'Smashed avo on a deliscious Sourdough toast', price: 5, image: 'https://picsum.photos/213' },
-    { id: 9, name: 'Iced Chocolate', description: 'Smashed avo on a deliscious Sourdough toast', price: 7, image: 'https://picsum.photos/214' },
-    { id: 10, name: 'Hot Chocolate', description: 'Smashed avo on a deliscious Sourdough toast', price: 4, image: 'https://picsum.photos/215' },
+    { id: 1, name: 'Flat White', description: 'Smashed avo on a deliscious Sourdough toast', price: 5, image: 'https://picsum.photos/200' },
+    { id: 2, name: 'Latter', description: 'Smashed avo on a deliscious Sourdough toast', price: 5, image: 'https://picsum.photos/200' },
+    { id: 3, name: 'Cappuchino', description: 'Smashed avo on a deliscious Sourdough toast', price: 5, image: 'https://picsum.photos/200' },
+    { id: 4, name: 'Long Black', description: 'Smashed avo on a deliscious Sourdough toast', price: 4, image: 'https://picsum.photos/200' },
+    { id: 5, name: 'Espresso', description: 'Smashed avo on a deliscious Sourdough toast', price: 3, image: 'https://picsum.photos/200' },
+    { id: 6, name: 'Doppio', description: 'Smashed avo on a deliscious Sourdough toast', price: 6, image: 'https://picsum.photos/200' },
+    { id: 7, name: 'Iced Latte', description: 'Smashed avo on a deliscious Sourdough toast', price: 5.5, image: 'https://picsum.photos/200' },
+    { id: 8, name: 'Iced Long black', description: 'Smashed avo on a deliscious Sourdough toast', price: 5, image: 'https://picsum.photos/200' },
+    { id: 9, name: 'Iced Chocolate', description: 'Smashed avo on a deliscious Sourdough toast', price: 7, image: 'https://picsum.photos/200' },
+    { id: 10, name: 'Hot Chocolate', description: 'Smashed avo on a deliscious Sourdough toast', price: 4, image: 'https://picsum.photos/200' },
 ]
 
 export default function DrinkDisplay() {
