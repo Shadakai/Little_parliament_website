0a0462148cc9c127a05e30f80cd748061bc80a79
Axel Whitford <axel@tanda.co>
2022-02-12 18:37:59 +1000

    netlify at it again

--- src/little-parliament-frontend/vite.config.js
+++ src/little-parliament-frontend/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-    rollupOptions: {
-      external: ['@mui','@mui/material/grid'],
-      }
-  },
+  //   build: {
+  //   rollupOptions: {
+  //     external: ['@mui','@mui/material/grid'],
+  //     }
+  // },
   plugins: [react()]
 })
