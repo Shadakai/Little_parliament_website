609fa8276ae8b8c75894cd0d7edd0a26ff78b674
Tia-Koenig <tiakoenig1994@gmail.com>
2022-02-13 16:53:09 +1000

    Possible fix for router 404

--- src/little-parliament-frontend/src/Router.jsx
+++ src/little-parliament-frontend/src/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Menu from "./pages/Menu"
@@ -46,6 +46,11 @@ function Router() {
       element: <Admin />,
       exact_path: false,
     },
+    {
+      path: "*",
+      element: <Navigate to="/" replace />,
+      exact_path: false,
+    }
   ]
 
   const routes = route_details.map((route, index) =>
